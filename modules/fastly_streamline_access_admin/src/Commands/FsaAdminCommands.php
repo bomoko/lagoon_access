59352fa0e66149424e64ded95ea981e9c2e05ddf
Blaize Kaye <blaize.kaye@amazee.com>
2021-10-01 16:42:19 +1300

    Removes desctructive constructive

--- modules/fastly_streamline_access_admin/src/Commands/FsaAdminCommands.php
+++ modules/fastly_streamline_access_admin/src/Commands/FsaAdminCommands.php
@@ -27,13 +27,6 @@ class FsaAdminCommands extends DrushCommands {
     'Comment',
   ];
 
-  public function __construct() {
-    parent::__construct();
-    if(empty(FsaFastlyDrupalUtilities::getApiKey()) || empty(FsaFastlyDrupalUtilities::getServiceId())) {
-      throw new \Exception("FSA has not been configured.");
-    }
-  }
-
   /**
    * Ages off IPs
    *
